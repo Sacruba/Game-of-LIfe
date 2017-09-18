123753b3f9428a0d22c8bcc65272ee0d59ac117b
Sebastian Ahuis <ahuiss@uni-bremen.de>
2017-09-18 16:01:19 +0200

    reafctoring Auslagern von Parametern in Variablen anstelle der impliziten Programmierung erlaubt später anpassung dieser parameter zur Laufzeit

--- src/Main/GameOfLife.java
+++ src/Main/GameOfLife.java
@@ -1,68 +1,88 @@
 package Main;
 
-public class GameOfLife implements IGameOfLife{
+public class GameOfLife implements IGameOfLife {
 
-		
-	
+	private int minLivingNeighboursforDead = 3;
+	private int minLivingNeighboursforAlive = 2;
+
+	private int maxLivingNeighboursforDead = 3;
+	private int maxLivingNeighboursforAlive = 3;
+
 	@Override
 	public boolean[][] gameOfLife(boolean[][] pPopulation, int generations) {
-		
-		//Exception checks
-		if (pPopulation == null){
+
+		// Exception checks
+		if (pPopulation == null) {
 			throw new IllegalArgumentException("There was no population");
-		}
-		else if (generations < 0){
+		} else if (generations < 0) {
 			throw new IllegalArgumentException("Generations can't be negative");
 		}
 		for (int i = 0; i < pPopulation.length; i++) {
-			if(pPopulation[i].length != pPopulation[1].length){
+			if (pPopulation[i].length != pPopulation[1].length) {
 				throw new IllegalArgumentException("The population must be rectangle shaped");
 			}
 		}
-		
+
 		boolean[][] population = pPopulation;
-		
-			//one iteration for each generation
-			for (int k=0 ;k < generations;k++){
-				boolean[][] newPopulation = new boolean[population.length][population[1].length];	
-				for (int i = 0; i < population.length; i++) {
-					for (int j = 0; j < population[i].length; j++){
-						
-						newPopulation[i][j] = suriveOrDie(population ,i ,j);	
-					}
+
+		// one iteration for each generation
+		for (int k = 0; k < generations; k++) {
+			boolean[][] newPopulation = new boolean[population.length][population[1].length];
+			for (int i = 0; i < population.length; i++) {
+				for (int j = 0; j < population[i].length; j++) {
+
+					newPopulation[i][j] = suriveOrDie(population, i, j);
 				}
-				population = newPopulation;
 			}
-			
+			population = newPopulation;
+		}
+
 		return population;
 	}
-	
+
+	public void setMinLivingNeighboursforDead(int newValue) {
+		minLivingNeighboursforDead = newValue;
+	}
+
+	public void setminLivingNeighboursforAlive(int newValue) {
+		minLivingNeighboursforAlive = newValue;
+	}
+
+	public void setmaxLivingNeighboursforDead(int newValue) {
+		maxLivingNeighboursforDead = newValue;
+	}
+
+	public void setmaxLivingNeighboursforAlive(int newValue) {
+		maxLivingNeighboursforAlive = newValue;
+	}
+
 	/*
-	 * Private Method used to check if one cell is alive or dead in the next Generation
+	 * Private Method used to check if one cell is alive or dead in the next
+	 * Generation
 	 */
-	private boolean suriveOrDie(boolean[][] population, int row, int column){
+	private boolean suriveOrDie(boolean[][] population, int row, int column) {
 		int livingNeighbours = countLivingNeighbours(population, row, column);
-	
-		if(livingNeighbours == 3){
+
+		if (!population[row][column] && minLivingNeighboursforDead <= livingNeighbours
+				&& livingNeighbours <= maxLivingNeighboursforDead) {
 			return true;
-		}
-		else if(livingNeighbours == 2 && population[row][column]){
+		} else if (population[row][column] && minLivingNeighboursforAlive <= livingNeighbours
+				&& livingNeighbours <= maxLivingNeighboursforAlive) {
 			return true;
-		}
-		else{
-		return false;
+		} else {
+			return false;
 		}
 	}
 
 	/*
 	 * Private Method used to count the living neighbours for the givne cell
 	 */
-	private int countLivingNeighbours(boolean[][] population, int row, int column){
+	private int countLivingNeighbours(boolean[][] population, int row, int column) {
 		int count = 0;
-		for (int i = -1;i < 2 ; i++){
-			for (int j = -1;j < 2 ; j++){
-				if (i!=0 || j!=0){
-					if (population[mod(row + i, population.length)][mod(column + j, population[row].length)]){
+		for (int i = -1; i < 2; i++) {
+			for (int j = -1; j < 2; j++) {
+				if (i != 0 || j != 0) {
+					if (population[mod(row + i, population.length)][mod(column + j, population[row].length)]) {
 						count++;
 					}
 				}
@@ -70,18 +90,16 @@ public class GameOfLife implements IGameOfLife{
 		}
 		return count;
 	}
-	
-	
+
 	/*
-	 * Private Method used to calcualte modulo as the % Operator can return negative Values
+	 * Private Method used to calcualte modulo as the % Operator can return
+	 * negative Values
 	 */
-	private int mod(int x, int y)
-	{
-	    int result = x % y;
-	    if (result < 0)
-	    {
-	        result += y;
-	    }
-	    return result;
+	private int mod(int x, int y) {
+		int result = x % y;
+		if (result < 0) {
+			result += y;
+		}
+		return result;
 	}
 }
