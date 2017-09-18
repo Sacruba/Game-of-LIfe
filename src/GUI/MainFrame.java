6f9b37ca625f182d979ced899f09dfd77107837e
Sebastian Ahuis <ahuiss@uni-bremen.de>
2017-09-18 15:47:39 +0200

    refactoring: Actionhandler in eigene Klassen asugelagert

--- src/GUI/MainFrame.java
+++ src/GUI/MainFrame.java
@@ -1,142 +1,104 @@
 package GUI;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import Main.GameOfLife;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
+public class MainFrame extends JFrame {
 
-public class MainFrame extends JFrame{
-	
 	private GameOfLife game;
 
 	private static final long serialVersionUID = 1L;
-	private static final int WIDTH = 400;
-    private static final int HEIGHT = 300;
-    
-    private JLabel generations;
-    private JTextField generationsTF;
-    private JTextField[][] table;
-    private JButton calculateB, exitB;
-    private JPanel[] menu;
-    
-    //Button handlers:
-    private CalculateButtonHandler cbHandler;
-    private ExitButtonHandler ebHandler;
+	private static final int WIDTH = 1400;
+	private static final int HEIGHT = 800;
 
+	private JLabel generations;
+	private JTextField generationsTF;
+	private JTextField[][] table;
+	private JButton calculateB, exitB;
+	private JPanel[] menu;
 
-    
-    public MainFrame(GameOfLife theGame)
-    {
-    	
-    	game = theGame;
+	// Button handlers:
+	private CalculateButtonHandler cbHandler;
+	private ExitButtonHandler ebHandler;
 
-        generations = new JLabel("Generations: ", SwingConstants.RIGHT);
-        generationsTF = new JTextField(10);
-    	
-        menu = new JPanel[10];
-        table = new JTextField[10][10];
-        
-        for (int i = 0; i < table.length; i++) {
-        		menu[i] = new JPanel();
-        	}
-         
-        //SPecify handlers for each button and add (register) ActionListeners to each button.
-        calculateB = new JButton("Calculate");
-        cbHandler = new CalculateButtonHandler();
-        calculateB.addActionListener(cbHandler);
-   
-        exitB = new JButton("Exit");
-        ebHandler = new ExitButtonHandler();
-        exitB.addActionListener(ebHandler);
-         
-        setTitle("The Game of Life");
-        
-        Container pane = getContentPane();
-        pane.setLayout(new GridLayout(10, 11));
-        
-        menu[1].add(generations);
-        menu[2].add(generationsTF);
-        menu[3].add(calculateB);
-        menu[9].add(exitB);
-        
-        for (int i = 0; i < table.length; i++) {
-        		pane.add(menu[i]);
-    		for (int j = 0; j < table[i].length; j++){
-    			table[i][j] = new JTextField(5);
-    			pane.add(table[i][j]);
-    			table[i][j].setText("Dead");
-    			table[i][j].setBackground(new Color(255, 100, 100));
-    			table[i][j].setHorizontalAlignment(JTextField.CENTER);
-    			table[i][j].setEnabled(false);
-    			table[i][j].setDisabledTextColor(new Color(0, 0, 0));
-    			}
-    		}
-        
-        table[1][2].setText("Alive");
-        table[3][2].setText("Alive");
-        table[2][1].setText("Alive");
-        table[3][3].setText("Alive");
-        table[4][4].setText("Alive");
-        
-        table[1][2].setBackground(new Color(100, 255, 100));
-        table[3][2].setBackground(new Color(100, 255, 100));
-        table[2][1].setBackground(new Color(100, 255, 100));
-        table[3][3].setBackground(new Color(100, 255, 100));
-        table[4][4].setBackground(new Color(100, 255, 100));
-        
-         
-         
-        setSize(WIDTH, HEIGHT);
-        setVisible(true);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-    }
-    
-    private class CalculateButtonHandler implements ActionListener
-    {
-        public void actionPerformed(ActionEvent e)
-        {
-           boolean[][] currentPopulation = new boolean[table.length][table[1].length];
-           boolean[][] newPopulation;
-           
-           for (int i = 0; i < table.length; i++) {
-       			for (int j = 0; j < table[i].length; j++){
-       				if (table[i][j].getText().equals("Alive")){
-       					currentPopulation[i][j] = true;
-       				}
-       				else
-       				{
-       					currentPopulation[i][j] = false;
-       				}
-       			}
-           }
-           
-           newPopulation = game.gameOfLife(currentPopulation, Integer.parseInt(generationsTF.getText()));
-            	
-           for (int i = 0; i < newPopulation.length; i++) {
-       			for (int j = 0; j < newPopulation[i].length; j++){
-       				if (newPopulation[i][j]){
-       					table[i][j].setText("Alive");
-       			        table[i][j].setBackground(new Color(100, 255, 100));
-       				}
-       				else
-       				{
-       					table[i][j].setText("Dead");
-       	    			table[i][j].setBackground(new Color(255, 100, 100));
-       				}
-       			}
-           }
-        }
-    }
-     
-    public class ExitButtonHandler implements ActionListener
-    {
-        public void actionPerformed(ActionEvent e)
-        {
-            System.exit(0);
-        }
-    }
+	@FXML
+	private AnchorPane ap;
 
+	public MainFrame(GameOfLife theGame) {
+
+		game = theGame;
+
+		generations = new JLabel("Generations: ", SwingConstants.RIGHT);
+		generationsTF = new JTextField(10);
+		generationsTF.setText("1");
+		generationsTF.setHorizontalAlignment(JTextField.CENTER);
+
+		menu = new JPanel[10];
+		table = new JTextField[10][10];
+
+		for (int i = 0; i < table.length; i++) {
+			menu[i] = new JPanel();
+		}
+
+		// SPecify handlers for each button and add (register) ActionListeners
+		// to each button.
+		calculateB = new JButton("Calculate");
+		cbHandler = new CalculateButtonHandler(game, table, generationsTF);
+		calculateB.addActionListener(cbHandler);
+
+		exitB = new JButton("Exit");
+		ebHandler = new ExitButtonHandler();
+		exitB.addActionListener(ebHandler);
+
+		setTitle("The Game of Life");
+
+		Container pane = getContentPane();
+		pane.setLayout(new GridLayout(10, 11));
+
+		menu[1].add(generations);
+		menu[2].add(generationsTF);
+		menu[3].add(calculateB);
+		menu[9].add(exitB);
+
+		for (int i = 0; i < table.length; i++) {
+			pane.add(menu[i]);
+			for (int j = 0; j < table[i].length; j++) {
+				table[i][j] = new JTextField(5);
+				pane.add(table[i][j]);
+				table[i][j].setText("Dead");
+				table[i][j].setBackground(new Color(255, 100, 100));
+				table[i][j].setHorizontalAlignment(JTextField.CENTER);
+				table[i][j].setEnabled(false);
+				table[i][j].setDisabledTextColor(new Color(0, 0, 0));
+			}
+		}
+
+		table[1][2].setText("Alive");
+		table[3][2].setText("Alive");
+		table[2][1].setText("Alive");
+		table[3][3].setText("Alive");
+		table[4][4].setText("Alive");
+
+		table[1][2].setBackground(new Color(100, 255, 100));
+		table[3][2].setBackground(new Color(100, 255, 100));
+		table[2][1].setBackground(new Color(100, 255, 100));
+		table[3][3].setBackground(new Color(100, 255, 100));
+		table[4][4].setBackground(new Color(100, 255, 100));
+
+		setSize(WIDTH, HEIGHT);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 }
-
--- src/Test/GameOfLifeTest.java
+++ src/Test/GameOfLifeTest.java
@@ -1,211 +1,191 @@
 package Test;
-import static org.junit.Assert.*;
-import org.hamcrest.Matcher;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import Main.GameOfLife;
 
-public class GameOfLifeTest{
-	
+public class GameOfLifeTest {
+
 	GameOfLife testSystem;
 	boolean[][] testPopulation1;
 	boolean[][] testPopulation2;
-	
+
 	/**
 	 * creates two dead populations
 	 */
 	@Before
 	public void setUp() {
-	testSystem = new GameOfLife();
-	testPopulation1 = new boolean[5][5];
-	testPopulation2 = new boolean[5][5];
-	for (int i = 0; i < testPopulation1.length; i++) {
-		for (int j = 0; j < testPopulation1[i].length; j++){
-			testPopulation1[i][j] = false;
-			testPopulation2[i][j] = false;
+		testSystem = new GameOfLife();
+		testPopulation1 = new boolean[5][5];
+		testPopulation2 = new boolean[5][5];
+		for (int i = 0; i < testPopulation1.length; i++) {
+			for (int j = 0; j < testPopulation1[i].length; j++) {
+				testPopulation1[i][j] = false;
+				testPopulation2[i][j] = false;
 			}
 		}
-	
+
 	}
-	
+
 	/**
 	 * Tests if a complete dead population stays dead
 	 */
 	@Test
-	public void testDeadPopulation(){
-	assertArrayEquals(testSystem.gameOfLife(testPopulation1, 1),testPopulation2);
-	assertArrayEquals(testSystem.gameOfLife(testPopulation1, 3),testPopulation2);
+	public void testDeadPopulation() {
+		assertArrayEquals(testSystem.gameOfLife(testPopulation1, 1), testPopulation2);
+		assertArrayEquals(testSystem.gameOfLife(testPopulation1, 3), testPopulation2);
 	}
-	
+
 	/**
-	 * Tests if a cell dies, when less then three neighbours are alive.
-	 * The Tested Population looks like:
+	 * Tests if a cell dies, when less then three neighbours are alive. The
+	 * Tested Population looks like:
 	 * 
-	 * 1 1 0 0 0
-	 * 0 0 0 1 0
-	 * 0 0 0 0 0
-	 * 0 0 0 0 0
-	 * 0 0 0 0 0
+	 * 1 1 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
 	 * 
 	 * All living cells shall die.
 	 */
 	@Test
-	public void testDeadThroughLowPopulation(){
+	public void testDeadThroughLowPopulation() {
 		testPopulation1[0][0] = true;
 		testPopulation1[0][1] = true;
 		testPopulation1[1][3] = true;
-		assertArrayEquals(testSystem.gameOfLife(testPopulation1, 1),testPopulation2);
-		
+		assertArrayEquals(testSystem.gameOfLife(testPopulation1, 1), testPopulation2);
+
 	}
-	
+
 	/**
-	 * Tests if a cell survives, when it has 2 living neighbours
-	 * The Tested Population looks like:
+	 * Tests if a cell survives, when it has 2 living neighbours The Tested
+	 * Population looks like:
 	 * 
-	 * 1 0 0 0 0
-	 * 0 1 0 0 0
-	 * 0 0 1 0 0
-	 * 0 0 0 1 0
-	 * 0 0 0 0 1
+	 * 1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 1
 	 * 
 	 * Its is expeted that all living cells survive and that no new cell lives.
 	 */
 	@Test
-	public void testSurvivalofLiving(){
+	public void testSurvivalofLiving() {
 		testPopulation1[0][0] = true;
 		testPopulation1[1][1] = true;
 		testPopulation1[2][2] = true;
 		testPopulation1[3][3] = true;
 		testPopulation1[4][4] = true;
-		
+
 		testPopulation2[0][0] = true;
 		testPopulation2[1][1] = true;
 		testPopulation2[2][2] = true;
 		testPopulation2[3][3] = true;
 		testPopulation2[4][4] = true;
-		
-		assertArrayEquals(testSystem.gameOfLife(testPopulation1, 1),testPopulation2);
-		assertArrayEquals(testSystem.gameOfLife(testPopulation1, 3),testPopulation2);
+
+		assertArrayEquals(testSystem.gameOfLife(testPopulation1, 1), testPopulation2);
+		assertArrayEquals(testSystem.gameOfLife(testPopulation1, 3), testPopulation2);
 	}
-	
+
 	/**
-	 * Tests if a cell starts living, when it has three living neighbours
-	 * The Tested Population looks like:
+	 * Tests if a cell starts living, when it has three living neighbours The
+	 * Tested Population looks like:
 	 * 
-	 * 0 1 0 0 0
-	 * 1 0 0 0 0
-	 * 0 0 1 0 0
-	 * 0 0 0 0 0
-	 * 0 0 0 0 0
+	 * 0 1 0 0 0 1 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0
 	 * 
 	 * Its is expeted that the cell 1/1 lives after one generation
 	 */
 	@Test
-	public void testNewLive(){
+	public void testNewLive() {
 		testPopulation1[0][1] = true;
 		testPopulation1[1][0] = true;
 		testPopulation1[2][2] = true;
-		
+
 		assertTrue(testSystem.gameOfLife(testPopulation1, 1)[1][1]);
 	}
-	
+
 	/**
-	 * Tests if a cell dies when it has more then three living neighbours.
-	 * The Tested Population looks like:
+	 * Tests if a cell dies when it has more then three living neighbours. The
+	 * Tested Population looks like:
 	 * 
-	 * 0 0 0 0 0
-	 * 0 0 1 0 0
-	 * 0 1 1 1 0
-	 * 0 0 1 0 0
-	 * 0 0 0 0 0
+	 * 0 0 0 0 0 0 0 1 0 0 0 1 1 1 0 0 0 1 0 0 0 0 0 0 0
 	 * 
 	 * Its is expeted that the cell 2/2 is dead after one generation
 	 */
 	@Test
-	public void testDeadThroughHighPopulation(){
+	public void testDeadThroughHighPopulation() {
 		testPopulation1[1][2] = true;
 		testPopulation1[3][2] = true;
 		testPopulation1[2][1] = true;
 		testPopulation1[2][3] = true;
 		testPopulation1[2][2] = true;
-		
+
 		assertFalse(testSystem.gameOfLife(testPopulation1, 1)[2][2]);
-		
+
 	}
-	
+
 	/**
-	 * Tests if a the population doesn't change when 0 generation shall be calculated
+	 * Tests if a the population doesn't change when 0 generation shall be
+	 * calculated
 	 */
 	@Test
-	public void testZeroGenerations(){
+	public void testZeroGenerations() {
 		testPopulation1[1][2] = true;
 		testPopulation1[3][2] = true;
 		testPopulation1[2][1] = true;
 		testPopulation1[3][3] = true;
 		testPopulation1[4][4] = true;
-		
+
 		testPopulation2[1][2] = true;
 		testPopulation2[3][2] = true;
 		testPopulation2[2][1] = true;
 		testPopulation2[3][3] = true;
 		testPopulation2[4][4] = true;
-		
-		assertArrayEquals(testSystem.gameOfLife(testPopulation1, 0),testPopulation2);
+
+		assertArrayEquals(testSystem.gameOfLife(testPopulation1, 0), testPopulation2);
 	}
-	
+
 	/**
-	 * Tests if a population does look like expected when multiple generations pass
-	 * The Tested Population looks like:
+	 * Tests if a population does look like expected when multiple generations
+	 * pass The Tested Population looks like:
 	 * 
-	 * 0 0 0 0 0
-	 * 0 0 1 0 0
-	 * 0 1 0 0 0
-	 * 0 0 1 1 0
-	 * 0 0 0 0 1
+	 * 0 0 0 0 0 0 0 1 0 0 0 1 0 0 0 0 0 1 1 0 0 0 0 0 1
 	 * 
 	 * This is how the population changes after one and two generations:
 	 * 
-	 * 0 0 0 0 0	0 0 0 0 0
-	 * 0 0 0 0 0	0 0 0 0 0
-	 * 0 1 0 1 0	0 0 0 1 0
-	 * 0 0 1 1 0	0 0 0 1 1
-	 * 0 0 0 1 0	0 0 1 1 0
+	 * 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 1 0 0 0 1 1 0 0 0
+	 * 0 1 1 0 0 0 1 0 0 0 1 1 0
 	 */
 	@Test
-	public void testMultipleGenerations(){
-		
+	public void testMultipleGenerations() {
+
 		testPopulation1[1][2] = true;
 		testPopulation1[3][2] = true;
 		testPopulation1[2][1] = true;
 		testPopulation1[3][3] = true;
 		testPopulation1[4][4] = true;
-		
+
 		testPopulation2[4][2] = true;
 		testPopulation2[2][3] = true;
 		testPopulation2[3][3] = true;
 		testPopulation2[4][3] = true;
 		testPopulation2[3][4] = true;
-		
-		assertArrayEquals(testSystem.gameOfLife(testPopulation1, 2),testPopulation2);
-		
+
+		assertArrayEquals(testSystem.gameOfLife(testPopulation1, 2), testPopulation2);
+
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
-	public void testNoPopulation(){
+	public void testNoPopulation() {
 		testSystem.gameOfLife(null, 1);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
-	public void testNegativeGeneration(){
+	public void testNegativeGeneration() {
 		testSystem.gameOfLife(testPopulation1, -3);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
-	public void testIncompatiblePopulationSize(){
-		boolean[][] testPopulation3 = {{true,true,true},{false,true,false},{false,false,false,false,false}};
+	public void testIncompatiblePopulationSize() {
+		boolean[][] testPopulation3 = { { true, true, true }, { false, true, false },
+				{ false, false, false, false, false } };
 		testSystem.gameOfLife(testPopulation3, 1);
 	}
 }
-
